3c441de9a7d9239b608b15c1bb3010010464f188
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-01-31 19:10:30 +0000

    makes some more stuff good

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // js main
 var x = document.getElementById("myFile");
-var myFirebaseRef = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/");
+var myFirebaseRef = new Firebase("https://chehacksregis.firebaseio.com/");
 
 function uploadFile() {
     var x = document.createElement("INPUT");
