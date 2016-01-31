53980047ed2a525c2893a6b15bf38d7fe3c65468
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-01-31 19:02:31 +0000

    added css

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // js main
+var x = document.getElementById("myFile");
+var myFirebaseRef = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/");
+
+function uploadFile() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "file");
+    document.body.appendChild(x);
+}
